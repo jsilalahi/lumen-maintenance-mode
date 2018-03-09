2e920b7e32e1094bf38c8557074f7bc53511b6d9
Jogi Silalahi <jsilalahi@dyned.com>
2018-03-09 14:35:15 +0700

    Update middleware

--- src/Http/Middleware/MaintenanceModeMiddleware.php
+++ src/Http/Middleware/MaintenanceModeMiddleware.php
@@ -20,7 +20,7 @@ class MaintenanceModeMiddleware {
      * @param  Laravel\Lumen\Application   $app
      * @return void
      */
-    public function __construct($app)
+    public function __construct(Application $app)
     {
         $this->app = $app;
     }
